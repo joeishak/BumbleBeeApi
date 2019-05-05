bd889380e93061df88bf6f382d1dd1710aae6536
Joseph Ishak <jishak1@asu.edu>
2019-05-05 15:19:12 -0700

    Making sure everything pushed

--- controllers/forms.js
+++ controllers/forms.js
@@ -10,7 +10,7 @@ pool.connect(err => {
     else console.log('connected to MySQL database:', config.database + 'on host: ' + config.host);
 });
 
-exports.getTypeNumVariants  = (req,res,next) =>{
+exports.getTypeNumVariants = (req, res, next) => {
     const query = `select distinct case 
 	when typenum = 'BS' then 'BS'
     when typenum = 'BSD' then 'BSD'
@@ -21,22 +21,22 @@ exports.getTypeNumVariants  = (req,res,next) =>{
     end as 'typenum'
     
      from egypt.elephant order by typenum`;
-     const variantQuery = ` select distinct     
-     case when    typenum != 'BS' and typenum != 'UNTY'  and typenum !='BSD' and typenum!= 'null' and   right(typenum,1)  REGEXP "[a-z, !]"  then right(typenum, 1) end as 'typeVariant'
-
+    const variantQuery = ` select distinct     
+     case when  typenum != 'BS' and typenum != 'UNTY'  and typenum !='BSD' and typenum!= 'null' and   right(typenum,1)  REGEXP "[a-z, !]"  then right(typenum, 1) end as 'typeVariant'
 from egypt.elephant;
 `
 
-     pool.query(query,(err,typeNumResponse,field)=>{
-         if(typeNumResponse){
-             pool.query(variantQuery, (err,variantResponse,fields)=>{
-                 let obj ={typeNum:  typeNumResponse, variants: variantResponse}
-                 res.send(obj);
-             })
-         }
-     })
+    pool.query(query, (err, typeNumResponse, field) => {
+        if (typeNumResponse) {
+            pool.query(variantQuery, (err, variantResponse, fields) => {
+                let obj = { typeNum: typeNumResponse, variants: variantResponse }
+                res.send(obj);
+            })
+        }
+    })
 }
-exports.writeElephantForms = (req,res,next) => {
+
+exports.writeElephantForms = (req, res, next) => {
     console.log(req.body.form)
     const form = req.body.form;
     const query = `INSERT INTO egypt.elephant (locusNum, objectGroupNum, objectNum, 
@@ -54,13 +54,13 @@ exports.writeElephantForms = (req,res,next) => {
             '${form.processedBy}','${form.processedDate}',
             '${form.enteredBy}','${form.enteredDate}',
             '${form.rlNum}','${form.sheetNum}'
-          );`;  
+          );`;
     console.log(query);
 
-     pool.query(query, (err, response, fields) => {
+    pool.query(query, (err, response, fields) => {
         // Success
         if (response) {
-            res.send({status: 201, OkPacket: response});
+            res.send({ status: 201, OkPacket: response });
         }
         if (err) {
             console.log(err);
@@ -75,7 +75,7 @@ exports.writeToKHPP = (req, res, next) => {
     const sherdsArr = req.body.sherds;
     const triageArr = req.body.triage;
 
- 
+
     console.log(sherdsArr);
 
 
@@ -87,53 +87,53 @@ exports.writeToKHPP = (req, res, next) => {
             console.log(response);
             const formId = response.insertId;
             // // TRIAGE
-           
+
             // Insert on bOdy
             const triageQueryArr = triageArr.map(triage => {
                 return `INSERT INTO egypt.khpptriage (formId, fabricType, bodyOrDiagnostic, count, weight, weightType, comments, notes,sherdType) VALUES ("${formId}", "${triage.fabricType}", "${triage.bodyOrDiagnostic}", "${triage.count}", "${triage.weight}", "${triage.weightType}", "${triage.comments}", "${triage.notes}", "${triage.sherdType}");`;
             });
             for (let i = 0; i < triageQueryArr.length; i++) {
                 const singleTriageQuery = triageQueryArr[i];
-                pool.query(singleTriageQuery, (err, response, fields) => { 
-                    if (response) { 
+                pool.query(singleTriageQuery, (err, response, fields) => {
+                    if (response) {
                         console.log("HOOOOOYYYY")
                     }
-                    if (err) { 
+                    if (err) {
                         console.log("OH NOOOO!!")
                     }
                 });
             }
 
-            if(sherdsArr.length!==0){
+            if (sherdsArr.length !== 0) {
                 const sherdsQueryArr = sherdsArr.map(sherds => {
-                    return  `INSERT INTO egypt.khppbodysherds(formid, fabricType, surfaceTreatment, comments, sherdType, count,  weight, weightType, notes) VALUES ("${formId}", "${sherds.fabricType}", "${sherds.surfaceTreatment}", "${sherds.comments}", "${sherds.sherdType}", "${sherds.count}", "${sherds.weight}", "${sherds.weightType}", "${sherds.notes}");`;
+                    return `INSERT INTO egypt.khppbodysherds(formid, fabricType, surfaceTreatment, comments, sherdType, count,  weight, weightType, notes) VALUES ("${formId}", "${sherds.fabricType}", "${sherds.surfaceTreatment}", "${sherds.comments}", "${sherds.sherdType}", "${sherds.count}", "${sherds.weight}", "${sherds.weightType}", "${sherds.notes}");`;
                 });
                 for (let j = 0; j < sherdsQueryArr.length; j++) {
                     const singleSherdQuery = sherdsQueryArr[j];
-                    pool.query(singleSherdQuery, (err, response, fields) => { 
-                        if (response) {    
+                    pool.query(singleSherdQuery, (err, response, fields) => {
+                        if (response) {
                             console.log("YESSS")
                         }
-                        if (err) {                 
+                        if (err) {
                             console.log(err)
                         }
                     });
                 }
             }
-            
 
-            res.send({status: 201, okPacket: response, message: 'INSERTS OKAY'});
-            
+
+            res.send({ status: 201, okPacket: response, message: 'INSERTS OKAY' });
+
         }
         if (err) {
-            res.send({status: 999, okPacket: response, message: 'ERROR IN INSERT'});
+            res.send({ status: 999, okPacket: response, message: 'ERROR IN INSERT' });
         }
     });
 
 }
 
 exports.readFromKHPP = (req, res, next) => {
-    
+
     const query = `select tagNumber, dueDate, processedBy, (select count(*) from egypt.khpptriage t where t.formid = f.id) as 'basicCount' , (select count(*) from egypt.khppbodysherds b where b.formId = f.id ) as 'detailedCount' from egypt.khppform f order by tagNumber;`;
 
     pool.query(query, (err, response, fields) => {
